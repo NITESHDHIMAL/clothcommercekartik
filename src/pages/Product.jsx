d0e2a74654318dff4e5768611f25ad253a55d805
NITESHDHIMAL <niteshdhimal18@gmail.com>
2024-12-12 10:40:32 +0545

    product data map

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,29 +4,29 @@ const Product = () => {
 
   const product = [
     {
-      name: "Apple",
+      name: null,
       price: "200",
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS_Db0jJvWe6vYScLksI8qoM2WCeHfJnSBVw&s"
     },
     {
       name: "Banana",
-      price: "200",
-      image: "apple"
+      price: "500",
+      image: "https://nutritionsource.hsph.harvard.edu/wp-content/uploads/2018/08/bananas-1354785_1920.jpg"
     },
     {
       name: "Orange",
-      price: "200",
-      image: "apple"
+      price: "600",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTct4QnA6zwU4tyn9vq_ADJ8Ey6RfIWK-6V6g&s"
     },
     {
       name: "Guava",
-      price: "200",
-      image: "apple"
+      price: "700",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBePRa9mErJGZpF7C9ZuD4RehKCcFyySQOEw&s"
     },
     {
       name: "Graps",
-      price: "200",
-      image: "apple"
+      price: "300",
+      image: "https://5.imimg.com/data5/SELLER/Default/2023/1/OG/JA/IJ/45117192/fresh-black-graps-500x500.jpg"
     },
   ]
 
@@ -41,155 +41,32 @@ const Product = () => {
 
           <div className="mt-10 grid grid-cols-2 gap-6 sm:grid-cols-4 sm:gap-4 lg:mt-16">
 
-            <article className="relative flex flex-col overflow-hidden rounded-lg border">
-              <div className="aspect-square overflow-hidden">
-                <img className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="/images/neZnfwBHi0f-4TivjA6BS.png" alt="" />
-              </div>
-              <div className="absolute top-0 m-2 rounded-full bg-white">
-                <p className="rounded-full bg-emerald-500 p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">Sale</p>
-              </div>
-              <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <div className="mb-2 flex">
-                  <p className="mr-3 text-sm font-semibold">$99.00</p>
-                  <del className="text-xs text-gray-400"> $79.00 </del>
+
+            {product?.map((data) => (
+              <>
+              <article className="relative flex flex-col overflow-hidden rounded-lg border">
+                <div className="aspect-square overflow-hidden">
+                  <img className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src={data?.image} alt="" />
                 </div>
-                <h3 className="mb-2 text-sm text-gray-400">Fresh Apples</h3>
-              </div>
-              <button className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
-                <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">Add</div>
-                <div className="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-emerald-500 group-hover:text-white">+</div>
-              </button>
-            </article>
-
-            <article className="relative flex flex-col overflow-hidden rounded-lg border">
-              <div className="aspect-square overflow-hidden">
-                <img className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="/images/exH8iOZwkjQekSALkXCZb.png" alt="" />
-              </div>
-              <div className="absolute top-0 m-2 rounded-full bg-white">
-                <p className="rounded-full bg-emerald-500 p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">Sale</p>
-              </div>
-              <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <div className="mb-2 flex">
-                  <p className="mr-3 text-sm font-semibold">$99.00</p>
-                  <del className="text-xs text-gray-400"> $79.00 </del>
+                <div className="absolute top-0 m-2 rounded-full bg-white">
+                  <p className="rounded-full bg-emerald-500 p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">Sale</p>
                 </div>
-                <h3 className="mb-2 text-sm text-gray-400">Fresh Apples</h3>
-              </div>
-              <button className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
-                <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">Add</div>
-                <div className="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-emerald-500 group-hover:text-white">+</div>
-              </button>
-            </article>
-
-            <article className="relative flex flex-col overflow-hidden rounded-lg border">
-              <div className="aspect-square overflow-hidden">
-                <img className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="/images/86WxAQqZpR5CqrhSPKwNv.png" alt="" />
-              </div>
-
-              <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <div className="mb-2 flex">
-                  <p className="mr-3 text-sm font-semibold">$99.00</p>
-                  <del className="text-xs text-gray-400"> $79.00 </del>
+                <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
+                  <div className="mb-2 flex">
+                    <p className="mr-3 text-sm font-semibold">Rs.{data?.price}</p>
+                    <del className="text-xs text-gray-400"> $79.00 </del>
+                  </div>
+                  <h3 className="mb-2 text-sm text-gray-400">{data?.name}</h3>
                 </div>
-                <h3 className="mb-2 text-sm text-gray-400">Fresh Apples</h3>
-              </div>
-              <button className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
-                <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">Add</div>
-                <div className="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-emerald-500 group-hover:text-white">+</div>
-              </button>
-            </article>
+                <button className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
+                  <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">Add</div>
+                  <div className="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-emerald-500 group-hover:text-white">+</div>
+                </button>
+              </article>
+              </>
+            ))}
 
-            <article className="relative flex flex-col overflow-hidden rounded-lg border">
-              <div className="aspect-square overflow-hidden">
-                <img className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="/images/0RV9Zy3daoeMbDmTOOMKm.png" alt="" />
-              </div>
 
-              <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <div className="mb-2 flex">
-                  <p className="mr-3 text-sm font-semibold">$99.00</p>
-                  <del className="text-xs text-gray-400"> $79.00 </del>
-                </div>
-                <h3 className="mb-2 text-sm text-gray-400">Fresh Apples</h3>
-              </div>
-              <button className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
-                <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">Add</div>
-                <div className="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-emerald-500 group-hover:text-white">+</div>
-              </button>
-            </article>
-
-            <article className="relative flex flex-col overflow-hidden rounded-lg border">
-              <div className="aspect-square overflow-hidden">
-                <img className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="/images/1DULzCtZvWhr5q5DDT6Ac.png" alt="" />
-              </div>
-
-              <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <div className="mb-2 flex">
-                  <p className="mr-3 text-sm font-semibold">$99.00</p>
-                  <del className="text-xs text-gray-400"> $79.00 </del>
-                </div>
-                <h3 className="mb-2 text-sm text-gray-400">Fresh Apples</h3>
-              </div>
-              <button className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
-                <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">Add</div>
-                <div className="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-emerald-500 group-hover:text-white">+</div>
-              </button>
-            </article>
-
-            <article className="relative flex flex-col overflow-hidden rounded-lg border">
-              <div className="aspect-square overflow-hidden">
-                <img className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="/images/neZnfwBHi0f-4TivjA6BS.png" alt="" />
-              </div>
-
-              <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <div className="mb-2 flex">
-                  <p className="mr-3 text-sm font-semibold">$99.00</p>
-                  <del className="text-xs text-gray-400"> $79.00 </del>
-                </div>
-                <h3 className="mb-2 text-sm text-gray-400">Fresh Apples</h3>
-              </div>
-              <button className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
-                <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">Add</div>
-                <div className="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-emerald-500 group-hover:text-white">+</div>
-              </button>
-            </article>
-
-            <article className="relative flex flex-col overflow-hidden rounded-lg border">
-              <div className="aspect-square overflow-hidden">
-                <img className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="/images/POklWwmmu0AtDZe5tNh9j.png" alt="" />
-              </div>
-
-              <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <div className="mb-2 flex">
-                  <p className="mr-3 text-sm font-semibold">$99.00</p>
-                  <del className="text-xs text-gray-400"> $79.00 </del>
-                </div>
-                <h3 className="mb-2 text-sm text-gray-400">Fresh Apples</h3>
-              </div>
-              <button className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
-                <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">Add</div>
-                <div className="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-emerald-500 group-hover:text-white">+</div>
-              </button>
-            </article>
-
-            <article className="relative flex flex-col overflow-hidden rounded-lg border">
-              <div className="aspect-square overflow-hidden">
-                <img className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125" src="/images/7ObHe1EuFqmsZmri6-ROD.png" alt="" />
-              </div>
-              <div className="absolute top-0 m-2 rounded-full bg-white">
-                <p className="rounded-full bg-emerald-500 p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">Sale</p>
-              </div>
-              <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <div className="mb-2 flex">
-                  <p className="mr-3 text-sm font-semibold">$99.00</p>
-                  <del className="text-xs text-gray-400"> $79.00 </del>
-                </div>
-                <h3 className="mb-2 text-sm text-gray-400">Fresh Apples</h3>
-              </div>
-              <button className="group mx-auto mb-2 flex h-10 w-10/12 items-stretch overflow-hidden rounded-md text-gray-600">
-                <div className="flex w-full items-center justify-center bg-gray-100 text-xs uppercase transition group-hover:bg-emerald-600 group-hover:text-white">Add</div>
-                <div className="flex items-center justify-center bg-gray-200 px-5 transition group-hover:bg-emerald-500 group-hover:text-white">+</div>
-              </button>
-            </article>
           </div>
         </div>
       </section>
